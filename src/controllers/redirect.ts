639e63bb9f90182760ef6e9f4f57682e0bfd4a87
Carlos isen <amra_akyra@hotmail.com>
2023-03-17 22:24:04 +0000

    feat: redirect created

--- src/controllers/redirect.ts
+++ src/controllers/redirect.ts
@@ -4,7 +4,7 @@ import { urlModel } from "../models/mongoSchemas"
 const redirect = async (req: Request, res: Response) => {
             const _id: string = req.params.url
             try{
-                const payload: any = await urlModel.findById(_id)
+                const payload: any = await urlModel.findByIdAndUpdate(_id, {$inc: {uses: 1}}, {returnDocument: "after"})
                 // hay que incluir un uso modificando el modelo.
                 if(payload.url){
                     // poner redireccion
@@ -15,6 +15,24 @@ const redirect = async (req: Request, res: Response) => {
                 // redirigir a una pagina oficial de error
                 return res.status(404).json({msg: "no existe esta direccion"})
             }
+
+}
+
+export const frontRedirect = async (req: Request, res: Response) => {
+    const _id: string = req.params.url
+    try {
+        const payload: any = await urlModel.findByIdAndUpdate(_id, { $inc: { uses: 1 } }, { returnDocument: "after" })
+        // hay que incluir un uso modificando el modelo.
+        if (payload.url) {
+            // poner redireccion
+            return res.status(200).json({redirect: payload.url})
+        }
+        throw Error("No existe el ID")
+    } catch (error: any) {
+        console.log(error)
+        // redirigir a una pagina oficial de error
+        return res.status(404).json({ msg: "no existe esta direccion" })
+    }
 
 }
 
