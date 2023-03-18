c1087bcf25343e5a22275d6f6a098036db036952
Carlos isen <amra_akyra@hotmail.com>
2023-03-18 09:37:09 +0000

    erase notes

--- src/controllers/redirect.ts
+++ src/controllers/redirect.ts
@@ -5,14 +5,11 @@ const redirect = async (req: Request, res: Response) => {
             const _id: string = req.params.url
             try{
                 const payload: any = await urlModel.findById(_id)
-                // hay que incluir un uso modificando el modelo.
                 if(payload.url){
-                    // poner redireccion
                 return res.status(200).redirect(payload.url)}
                 throw Error("No existe el ID")
             }catch(error: any){
                 console.log(error)
-                // redirigir a una pagina oficial de error
                 return res.status(404).json({msg: "no existe esta direccion"})
             }
 
